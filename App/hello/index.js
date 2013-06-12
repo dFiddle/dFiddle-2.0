d8b872804dc2fc4c26726813e56094e915995504
RainerAtSpirit <R.Wittmann@spirit.de>
2013-06-12 11:54:13 +0200

    update to Durandal 06/12

---
+++ App/hello/index.js
@@ -0,0 +1,24 @@
+﻿define(['plugins/router', 'global'], function( router, global ) {
+    var childRouter = router.createChildRouter()
+      .makeRelative({
+          moduleId: 'hello',
+          route: 'hello'
+      }).map([
+          {route: '', moduleId: 'default/index', title: 'Hello World', type: 'intro'},
+          {route: 'default', moduleId: 'default/index', title: 'Hello World', type: 'intro', nav: true},
+          {route: 'dFiddle', moduleId: 'dFiddle/index', title: 'Hello World', type: 'fiddle', nav: true}
+      ]).buildNavigationModel();
+
+    // chaining .on breaks childRouter for whatever reason
+    childRouter.on('router:navigation:complete').then(global.createSampleLink);
+
+    return {
+        global: global,
+        router: childRouter,
+        getItemsByCategoryId: function( categoryId ) {
+            return ko.utils.arrayFilter(childRouter.navigationModel(), function( route ) {
+                return route.type === categoryId;
+            });
+        }
+    };
+});
--- App/main.js
+++ App/main.js
@@ -1,21 +1,12 @@
 ﻿requirejs.config({
     paths: {
-      'text': '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text.min'
+      'text': '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text.min',
+      'plugins' : '../lib/durandal/js/plugins',
+      'transitions' : '../lib/durandal/js/transitions'
     }
 });
 
-define(['durandal/app', 'durandal/system', 'durandal/viewLocator'],
-  function (app, system, viewLocator) {
-    
-    system.debug(true);
-    
-    app.title = 'Durandal Samples';
-    app.start().then(function () {
-        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-        //Look for partial views in a 'views' folder in the root.
-        viewLocator.useConvention();
 
-        app.adaptToDevice();
-        app.setRoot('samples/shell');
-    });
+define(['../lib/durandal/js/durandal'], function(){
+    require(['bootstrapper']);
 });
