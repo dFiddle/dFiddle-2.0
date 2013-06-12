b48896cc18edb5874e55471ef1231f4accd060c9
RainerAtSpirit <R.Wittmann@spirit.de>
2013-06-12 12:07:59 +0200

    removed apptemp dir

--- app/appTemp/masterDetail/wizard/step1.js
+++
@@ -1,9 +0,0 @@
-﻿define(function() {
-
-    return function() {
-        this.name = 'Step 1';
-        this.s1one = 'Unique to' + this.name;
-        this.s1two = 'Another property unique to' + this.name;
-    };
-
-});
--- app/appTemp/masterDetail/wizard/step2.js
+++
@@ -1,8 +0,0 @@
-﻿define(function() {
-
-    return function() {
-        this.name = 'Step 2';
-        this.property = 'Unique to' + this.name;
-    };
-
-});
