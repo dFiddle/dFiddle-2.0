89fb24c8593118008aaa0565e36474b286c7aa7e
RainerAtSpirit <R.Wittmann@spirit.de>
2013-06-13 09:20:01 +0200

    update to use AMD modules instead of single durandal.js file

--- app/hello/index.js
+++ app/hello/index.js
@@ -7,7 +7,7 @@
           {route: '', moduleId: 'default/index', title: 'Hello World', type: 'intro'},
           {route: 'default', moduleId: 'default/index', title: 'Hello World', type: 'intro', nav: true},
           {route: 'dFiddle', moduleId: 'dFiddle/index', title: 'Hello World', type: 'fiddle', nav: true}
-      ]).buildNavigationModel()
+      ]).buildNavigationModel();
       
     // .on is mixed in an not meant to be  chainable 
     childRouter.on('router:navigation:complete').then(global.createSampleLink);
