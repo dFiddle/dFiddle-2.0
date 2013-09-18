cf83a59a2b7fcc4d57fdd073b31dd47f3f224b77
RainerAtSpirit <R.Wittmann@spirit.de>
2013-09-18 11:53:05 +0200

    fixed val -> value. update prop values

--- app/masterDetail/wizard2/index.js
+++ app/masterDetail/wizard2/index.js
@@ -9,11 +9,11 @@
         // Get wizard data based on wizId from the backend
         var json =
             {"id": "wizard001", "mode": "create", "steps": [
-                {"id": "s001", "name": "step one", "props": {"prop1": "a", "prop2": "b"}},
-                {"id": "s002", "name": "step twoe", "props": {"prop3": "a", "prop4": "b"}},
-                {"id": "s003", "name": "step three", "props": {"prop5": "a", "prop6": "b"}},
-                {"id": "s004", "name": "step four", "props": {"prop7": "a", "prop8": "b"}},
-                {"id": "s005", "name": "step five", "props": {"prop9": "a", "prop10": "b"}}
+                {"id": "s001", "name": "step one", "props": {"prop1": "one", "prop2": "Donald"}},
+                {"id": "s002", "name": "step twoe", "props": {"prop3": "two", "prop4": "Daisy"}},
+                {"id": "s003", "name": "step three", "props": {"prop5": "three", "prop6": "Daphne"}},
+                {"id": "s004", "name": "step four", "props": {"prop7": "four", "prop8": "Dagobert"}},
+                {"id": "s005", "name": "step five", "props": {"prop9": "five", "prop10": "Della"}}
             ]};
 
         this.activeScreen(new Wizard(json));
